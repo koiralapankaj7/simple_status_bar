578507d42d04bf78e0483a3abdeaeb9fa12628e6
koiralapankajgit <koiralapankaj007@gmail.com>
2020-04-10 22:11:25 +0545

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_status_bar/simple_status_bar.dart';
+import 'package:simple_status_bar/simple_drawer.dart';
 
 void main() => runApp(MyApp());
 
