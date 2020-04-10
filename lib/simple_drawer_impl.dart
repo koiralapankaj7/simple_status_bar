e1fce35f09066c5af611edadec1d7be70ab1e32b
koiralapankajgit <koiralapankaj007@gmail.com>
2020-04-10 09:53:39 +0545

    simple drawer

---
+++ lib/simple_drawer_impl.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+typedef _DrawerCallback = void Function(bool opened);
+
+class SimpleDrawer extends StatefulWidget {
+  final _DrawerCallback drawerCallback;
+  final Widget child;
+  final double width;
+  final Color background;
+
+  const SimpleDrawer({
+    Key key,
+    this.drawerCallback,
+    @required this.child,
+    this.width,
+    this.background,
+  })  : assert(child != null, 'Simple Drawer child cannot be null.'),
+        super(key: key);
+
+  @override
+  _SimpleDrawerState createState() => _SimpleDrawerState();
+}
+
+class _SimpleDrawerState extends State<SimpleDrawer> {
+  @override
+  void initState() {
+    super.initState();
+    widget.drawerCallback(true);
+  }
+
+  @override
+  void dispose() {
+    widget.drawerCallback(false);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Container(
+          width: widget.width ?? constraints.maxWidth * 0.8,
+          color: widget.background ?? Colors.white,
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
