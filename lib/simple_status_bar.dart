78e4e9d951e945166e04c7bb6019f5cc438bec17
koiralapankajgit <koiralapankaj007@gmail.com>
2020-04-11 23:22:27 +0545

    status bar color with auto brightness

--- lib/simple_status_bar.dart
+++ lib/simple_status_bar.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 import 'package:flutter/services.dart';
 
 class SimpleStatusBar {
@@ -12,14 +13,30 @@ class SimpleStatusBar {
     });
   }
 
-  static Future isDarkModeOn() async {
-    _channel.invokeMethod('isDarkModeOn');
+  static Future<SystemTheme> getSystemTheme() async {
+    final int type = await _channel.invokeMethod('getSystemTheme');
+    return type == 1 ? SystemTheme.LIGHT : SystemTheme.DARK;
+  }
+
+  static Future changeColor({@required Color color, double value}) async {
+    if (color == null)
+      throw Exception(["Exception occured while changing color. Reason => Color cannot be null."]);
+    final double luminance = color.computeLuminance();
+    final bool isDarkColor = luminance < (value ?? 0.4);
+
+    print(
+        "Computing Color Luminance : Luminance is $luminance ==================> DARK : $isDarkColor");
+
+    await _channel.invokeMethod('changeColor', {'color': color.value, 'isDarkColor': isDarkColor});
   }
 
   // listen theme state
+  // changeColor
 
   // decorate status bar
 }
+
+enum SystemTheme { LIGHT, DARK, SYSTEM_DEFAULT, BATTERY_SAVER }
 
 enum StatusBarAnimation { SLIDE, FADE, NONE }
 
