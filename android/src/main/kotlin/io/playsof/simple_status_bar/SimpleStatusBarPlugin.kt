04ccd2a1788d06097838a0aa2e21ab39de083f03
koiralapankajgit <koiralapankaj007@gmail.com>
2020-04-11 23:21:52 +0545

    change status bar color with brightness

--- android/src/main/kotlin/io/playsof/simple_status_bar/SimpleStatusBarPlugin.kt
+++ android/src/main/kotlin/io/playsof/simple_status_bar/SimpleStatusBarPlugin.kt
@@ -1,10 +1,17 @@
 package io.playsof.simple_status_bar
 
+import android.R.color
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
 import android.util.Log
+import android.view.View
 import android.view.WindowManager
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -14,13 +21,14 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.lang.Exception
+
 
 /** SimpleStatusBarPlugin */
-public class SimpleStatusBarPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+class SimpleStatusBarPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     //
 
-    private var activity: Activity? = null
+    private lateinit var activity: Activity
+    private lateinit var context: Context
 
 
     override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
@@ -30,21 +38,20 @@ public class SimpleStatusBarPlugin : FlutterPlugin, MethodCallHandler, ActivityA
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val  instance: SimpleStatusBarPlugin = SimpleStatusBarPlugin()
+            val instance = SimpleStatusBarPlugin()
             instance.activity = registrar.activity()
             instance.onAttachedToEngine(registrar.context(), registrar.messenger())
         }
     }
 
-    fun onAttachedToEngine(context: Context, binaryMessenger: BinaryMessenger){
+    fun onAttachedToEngine(context: Context, binaryMessenger: BinaryMessenger) {
+        this.context = context
         val channel = MethodChannel(binaryMessenger, "simple_status_bar")
         channel.setMethodCallHandler(this)
     }
 
 
-
     override fun onDetachedFromActivity() {
-
     }
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
@@ -60,33 +67,37 @@ public class SimpleStatusBarPlugin : FlutterPlugin, MethodCallHandler, ActivityA
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
 
-        if(call.method == "toggleStatusBar") {
-            toggleStatusBar(call, result);
+
+        if (call.method == "toggleStatusBar") {
+            toggleStatusBar(call, result)
             return
         }
+
+        if (call.method == "getSystemTheme") {
+            getSystemTheme(call, result)
+            return
+        }
+
+        if (call.method == "changeColor") {
+            changeColor(call, result)
+            return
+        }
+
 
         result.notImplemented()
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     private fun toggleStatusBar(call: MethodCall, result: Result) {
-        if (this.activity == null) {
-            Log.e("SimpleStatusBar", "SimpleStatusBar: Activity is null cannot hide status bar.")
-            result.success(false)
-            return
-        }
-
         try {
-            val hidden : Boolean? = call.argument<Boolean>("hide")
+            val hidden: Boolean = call.argument<Boolean>("hide")!!
 
-            if (hidden!!) {
-                this.activity!!.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                this.activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-            } else {
-                this.activity!!.window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-                this.activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-            }
+            val hideFlag = this.activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_FULLSCREEN
+            val showFlag = this.activity.window.decorView.systemUiVisibility and  View.SYSTEM_UI_FLAG_FULLSCREEN.inv()
+            this.activity.window.decorView.systemUiVisibility = if (hidden)  hideFlag else showFlag
 
             result.success(true)
 
@@ -95,191 +106,76 @@ public class SimpleStatusBarPlugin : FlutterPlugin, MethodCallHandler, ActivityA
             result.success(false)
         }
 
+    }
+
+    private fun getSystemTheme(call: MethodCall, result: Result) {
+        try {
+//            MODE_NIGHT_AUTO_BATTERY
+            when (this.activity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    result.success(1)
+                    return
+                } // Night mode is not active, we're using the light theme
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    result.success(2)
+                    return
+                }
+                else -> {
+                    result.error("404", "Not found", "")
+                }
+            }
+
+        } catch (exception: Exception) {
+            Log.e("SimpleStatusBar", "SimpleStatusBar: Failed to get system theme : $exception")
+            result.error("500", "Something went wrong", "")
+        }
 
 
+    }
 
+    private fun changeColor(call: MethodCall, result: Result) {
+        try {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val isDarkColor: Boolean = call.argument<Boolean>("isDarkColor")!!
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+                    val lightStatusBarFlag : Int = this.activity.window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    val darkStatusBarFlag: Int = this.activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                    this.activity.window.decorView.systemUiVisibility = if (isDarkColor)  darkStatusBarFlag else lightStatusBarFlag
+                }
+
+                val startColor: Int = this.activity.window.statusBarColor
+                val endColor: Int = call.argument<Int>("color")!!
+
+                val valueAnimator : ValueAnimator = ValueAnimator.ofObject(ArgbEvaluator(), startColor, endColor)
+                valueAnimator.addUpdateListener {
+                    this.activity.window.statusBarColor = valueAnimator.animatedValue as Int
+                }
+                valueAnimator.setDuration(300).startDelay = 0
+                valueAnimator.start()
+
+            } else {
+                result.error("500", "Device not supported", "")
+            }
+//
+        } catch (exception: Exception) {
+            Log.e("SimpleStatusBar", "SimpleStatusBar: Failed to get system theme : $exception")
+            result.error("500", "Something went wrong", "")
+        }
+    }
+
+    private fun styleWith(call: MethodCall, result: Result) {
+        try {
+
+
+        } catch (exception: Exception) {
+            Log.e("SimpleStatusBar", "SimpleStatusBar Exception : $exception")
+            result.error("500", "Something went wrong", "")
+        }
     }
 
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     }
 }
-
-
-//package io.playsof.simple_status_bar
-//
-//import android.app.Activity
-//import android.content.Context
-//import androidx.annotation.NonNull;
-//import io.flutter.embedding.engine.plugins.FlutterPlugin
-//import io.flutter.embedding.engine.plugins.activity.ActivityAware
-//import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-//import io.flutter.plugin.common.BinaryMessenger
-//import io.flutter.plugin.common.MethodChannel
-//
-///** SimpleStatusBarPlugin */
-//public class SimpleStatusBarPlugin : FlutterPlugin, ActivityAware {
-//    //
-//
-//    private var channel: MethodChannel? = null
-//    private var handler: MethodCallHandlerImpl? = null
-//
-//    private val CHANNELID : String = "simple_status_bar"
-//
-//    private var activity: Activity? = null
-//
-//
-//    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-////        val channel = MethodChannel(flutterPluginBinding.binaryMessenger, "simple_status_bar")
-////        channel.setMethodCallHandler(this)
-////        setupChannel(binding.binaryMessenger, binding.applicationContext, null!!)
-//    }
-//
-//    override fun onDetachedFromActivity() {
-//        handler?.setActivity(null)
-//    }
-//
-//    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-//        onAttachedToActivity(binding)
-//    }
-//
-//    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-////        this.activity = binding.activity
-//        handler?.setActivity(binding.activity)
-//    }
-//
-//    override fun onDetachedFromActivityForConfigChanges() {
-//        onDetachedFromActivity()
-//    }
-//
-//
-////    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-////
-////
-////
-////        if(call.method == "toggleStatusBar") {
-////            toggleStatusBar(call, result);
-////            return
-////        }
-////
-////        if (call.method == "onDarkMode") {
-////            when (this.activity?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
-////                Configuration.UI_MODE_NIGHT_NO -> {}
-////                Configuration.UI_MODE_NIGHT_YES -> {}
-////            }
-////            return
-////        }
-////
-////        result.notImplemented()
-////    }
-//
-////    private fun toggleStatusBar(call: MethodCall, result: Result) {
-////        if (this.activity == null) {
-////            Log.e("SimpleStatusBar", "SimpleStatusBar: Activity is null cannot hide status bar.")
-////            result.success(false)
-////            return
-////        }
-////
-////        try {
-////            val hidden : Boolean? = call.argument<Boolean>("hide")
-////
-////            if (hidden!!) {
-////                this.activity!!.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-////                this.activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-////            } else {
-////                this.activity!!.window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-////                this.activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-////            }
-////
-////            result.success(true)
-////
-////        } catch (exception: Exception) {
-////            Log.e("SimpleStatusBar", "SimpleStatusBar: Failed to hide status bar : $exception")
-////            result.success(false)
-////        }
-////
-////
-////
-////
-////    }
-//
-//
-//    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-//        teardownChannel()
-//    }
-//
-//    private fun setupChannel(messenger: BinaryMessenger, context:Context, activity:Activity) {
-//        channel = MethodChannel(messenger, CHANNELID)
-//        handler = MethodCallHandlerImpl(context, activity)
-//        channel!!.setMethodCallHandler(handler)
-//    }
-//
-//
-//    private fun teardownChannel() {
-//        channel?.setMethodCallHandler(null)
-//        channel = null
-//        handler = null
-//    }
-//
-//    companion object {
-//        private val CHANNEL_ID = "plugins.flutter.io/quick_actions"
-////        fun registerWith(registrar: PluginRegistry.Registrar) {
-////            val plugin = QuickActionsPlugin()
-////            plugin.setupChannel(registrar.messenger(), registrar.context(), registrar.activity())
-////        }
-//    }
-//
-//}
-//
-//
-//
-////package io.flutter.plugins.quickactions
-////import android.app.Activity
-////import android.content.Context
-////import io.flutter.embedding.engine.plugins.FlutterPlugin
-////import io.flutter.embedding.engine.plugins.activity.ActivityAware
-////import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-////import io.flutter.plugin.common.BinaryMessenger
-////import io.flutter.plugin.common.MethodChannel
-////import io.flutter.plugin.common.PluginRegistry.Registrar
-/////** QuickActionsPlugin */
-////class QuickActionsPlugin:FlutterPlugin, ActivityAware {
-////    private val channel:MethodChannel
-////    private val handler:MethodCallHandlerImpl
-//
-////    fun onAttachedToEngine(binding:FlutterPluginBinding) {
-////        setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext(), null!!)
-////    }
-////    fun onDetachedFromEngine(binding:FlutterPluginBinding) {
-////        teardownChannel()
-////    }
-////    fun onAttachedToActivity(binding:ActivityPluginBinding) {
-////        handler.setActivity(binding.getActivity())
-////    }
-////    fun onDetachedFromActivity() {
-////        handler.setActivity(null)
-////    }
-////    fun onReattachedToActivityForConfigChanges(binding:ActivityPluginBinding) {
-////        onAttachedToActivity(binding)
-////    }
-////    fun onDetachedFromActivityForConfigChanges() {
-////        onDetachedFromActivity()
-////    }
-////    private fun setupChannel(messenger:BinaryMessenger, context:Context, activity:Activity) {
-////        channel = MethodChannel(messenger, CHANNEL_ID)
-////        handler = MethodCallHandlerImpl(context, activity)
-////        channel.setMethodCallHandler(handler)
-////    }
-////    private fun teardownChannel() {
-////        channel.setMethodCallHandler(null)
-////        channel = null
-////        handler = null
-////    }
-////    companion object {
-////        private val CHANNEL_ID = "plugins.flutter.io/quick_actions"
-////        fun registerWith(registrar:Registrar) {
-////            val plugin = QuickActionsPlugin()
-////            plugin.setupChannel(registrar.messenger(), registrar.context(), registrar.activity())
-////        }
-////    }
-////}
