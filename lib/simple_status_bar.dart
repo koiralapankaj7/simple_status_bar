721e54e6cfddbb2a50891180e849762420fb306a
koiralapankajgit <koiralapankaj007@gmail.com>
2020-08-10 16:24:26 +0545

    pub updated and license added

--- lib/simple_status_bar.dart
+++ lib/simple_status_bar.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter/services.dart';
 
 class SimpleStatusBar {
